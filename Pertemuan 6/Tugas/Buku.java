3d5943731260f3de4a5e4d457b784130a0078947
Akbar Indra Saputra <akbar.indra.saputra@students.untidar.ac.id>
2024-09-29 13:43:14 +0700

    Add files via upload

---
+++ Pertemuan 6/Tugas/Buku.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPraktikum6;
+
+/**
+ *
+ * @author AKBAR
+ */
+class Buku extends Produk {
+
+    public Buku(String nama, double harga) {
+        super(nama, harga);
+    }
+
+    @Override
+    public double hitungDiskon() {
+        return harga * 0.1;
+    }
+}
