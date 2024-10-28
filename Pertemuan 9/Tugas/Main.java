88f019bcbe6ddcf2c1750f2a3e787ca6538251be
Akbar Indra Saputra <akbar.indra.saputra@students.untidar.ac.id>
2024-10-28 20:00:12 +0700

    Add files via upload

---
+++ Pertemuan 9/Tugas/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPraktikum9;
+
+/**
+ *
+ * @author AKBAR
+ */
+public class Main {
+    public static void main(String[] args) {
+        Hewan anjing = new Anjing();
+        Hewan kucing = new Kucing();
+        
+        anjing.suara();
+        kucing.suara();
+    }
+}
